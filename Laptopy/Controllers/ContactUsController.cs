3bc5c0cb5b01dfa82dcefdf6a8de03cd950bba20
MohamedHassanein96 <mohamedhassanein365@gmail.com>
2024-11-09 16:44:27 +0200

    add ContactUs Controller

---
+++ Laptopy/Controllers/ContactUsController.cs
@@ -0,0 +1,41 @@
+﻿using LaptopyCore.DTO;
+using LaptopyCore.IUnitOfWorkRepository;
+using LaptopyCore.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laptopy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactUsController : ControllerBase
+    {
+        private readonly IUnitOfWorkRepository _unitOfWorkRepository;
+
+        public ContactUsController(IUnitOfWorkRepository unitOfWorkRepository)
+        {
+            _unitOfWorkRepository = unitOfWorkRepository;
+        }
+        [HttpPost("Create")]
+        public IActionResult Create(ContactUs contactUs)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWorkRepository.ContactUs.Create(contactUs);
+                _unitOfWorkRepository.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet("GetAll")]
+        public IActionResult Index()
+
+        {
+            var contactUs = _unitOfWorkRepository.ContactUs.Get();
+            return Ok(contactUs);
+        }
+    }
+}
